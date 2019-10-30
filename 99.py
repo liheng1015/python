33d514bba7ee44718a40fcdd3d1b652842d5aa96
liheng1015 <53298133+liheng1015@users.noreply.github.com>
2019-10-30 06:50:28 -0500

    函数99乘法表

---
+++ 99.py
@@ -0,0 +1,11 @@
+def matable(n=9):
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print('%sX%s=%s' %(j,i,j*i) , end=' ')
+        print()
+if __name__ == '__main__':
+    m=input('请输入乘法表个数(默认9):')
+    if m:
+        m=int(m)
+        matable(m)
+    else:
